ae0119aac20531fb08c51402394fa01efcefdc54
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-07-30 14:17:19 -0700

    moved text

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+
+const hiddenText = document.querySelectorAll(".text")
+
+// when you click on the button change hiddenText to white
+
+// and change all div backgrounds to black with blue borders? 
+
+
 // link btn 
 const btn = document.getElementById("btn")
+
 
 btn.onclick(() => {
     
