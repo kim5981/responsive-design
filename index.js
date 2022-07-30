34a36d64e6a0b7ce5bc35209358ccba656026e22
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-07-30 16:52:45 -0700

    variables

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 
 const hiddenText = document.querySelectorAll(".text")
+const btnWrap = document.querySelector("#btn")
+
+const topSectionWrap = document.querySelector(".top")
+const row1 = document.querySelectorAll(".row-1")
+
+const midSectionWrap = document.querySelector(".mid")
+const row2 = document.querySelectorAll(".row-2")
+
+const bottomSectionWrap = document.querySelector(".bottom")
+const row3 = document.querySelector(".row-3")
 
 // when you click on the button change hiddenText to white
 
